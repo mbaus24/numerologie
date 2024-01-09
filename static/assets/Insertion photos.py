79ae078fcd84faa5020f956d0571848afd3706d8
mbaus <mbaus@centrale-marseille.fr>
2024-01-09 10:38:30 +0100

    Ajout Photos

---
+++ static/assets/Insertion photos.py
@@ -0,0 +1,16 @@
+for i in range(1,116):
+    print("<div class=\"col-lg-4 col-md-4 col-sm-6 col-xs-12 all paphos\"> <div class=\"item\"> <a href=\"assets/images/PXC/Paphos/Paphos ("+str(i)+").jpg\" data-lightbox=\"image-1\" data-title=\"Sony a7III + Tamron 28-75mm f/2.8 \"><img src=\"assets/images/PXC/Paphos/Paphos ("+str(i)+").jpg\" alt=\"\"></a></div> </div>")
+
+
+
+
+#Changer: 
+#nombre photos
+
+# all catégorie a changer 
+
+# Répertoire
+
+#format image si jamais
+
+#Titre
---
+++ static/assets/js/snippets.js
@@ -0,0 +1,32 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+
+
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Ceci est un message d alerte', 'success')
+  })
+}
